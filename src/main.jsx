3ab06ea37645ba546d9c21aeba08d245f2130454
Ceo-Sammarco <noah.kd@gmail.com>
2023-05-05 08:26:47 -0300

    Ceo - sex 05 mai 2023 08:26:47 -03

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+
+export default function App() {
+    
+
+    return( <>
+        Céo Sammarco
+    </> );
+}
+
+
+ReactDOM.createRoot( document.querySelector( "main" ) ).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
